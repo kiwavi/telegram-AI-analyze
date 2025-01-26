44c4c6a73cc4512f67d2ec209f704d3b56c57efb
Kiwavi <dekamotho@gmail.com>
2025-01-26 17:09:59 +0300

    chore integrate-with-openai: minimal changes

--- utils/openai.js
+++ utils/openai.js
@@ -15,15 +15,9 @@ const completion = openai.chat.completions.create({
   messages: [
     {
       role: "user",
-      content: `Does the sentence 'An Israeli drone drops a bomb on a building in northern Gaza, flies off, and detonates it near Palestinian civilians, footage shared by local media shows' represent an act of aggression against Palestine by Israel? Answer with yes or no or maybe.`,
+      content: `Does the sentence 'An Israeli drone drops a bomb on a building in northern Gaza, flies off, and detonates it near Palestinian civilians, footage shared by local media shows' revolve around the Israel-Palestine conflict? Answer with yes or no or maybe.`,
     },
   ],
 });
-
-// let names = `Israeli military raid area surrounding hospital in West Bank - Sticking in the boots right before ceasefire — reports
-
-// #Israel`;
-
-// console.log(names.replace(/[\r\n]+/gm, " "));
 
 completion.then((result) => console.log(result.choices[0].message));
