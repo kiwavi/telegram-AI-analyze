6069a184d8d3e4966e35c634232828fb53d81f8a
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-06-12 08:05:14 +0300

    feat: add endpoint

--- index.ts
+++ index.ts
@@ -1,59 +1,44 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
-import { z, ZodError } from "zod";
+import { channels } from "./src/db/schema.js";
+import { messages } from "./src/db/schema.js";
+import { sql, eq } from "drizzle-orm";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
 import { drizzle } from "drizzle-orm/node-postgres";
-import { StatusCodes } from "http-status-codes";
 
 export const db = drizzle(process.env.DATABASE_URL!);
-
-const searchSchema = z.array(z.string());
-
-function validateData(schema: z.ZodArray<any, any>) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse(req.body.tags);
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const errorMessages = error.errors.map((issue: any) => ({
-          message: `${issue.path.length ? issue.path.join(".") : "Value"} is ${
-            issue.message
-          }`,
-        }));
-        res
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ error: "Invalid data", details: errorMessages });
-      } else {
-        res
-          .status(StatusCodes.INTERNAL_SERVER_ERROR)
-          .json({ error: "Internal Server Error" });
-      }
-    }
-  };
-}
+import bodyParser from "body-parser";
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello there");
 });
 
-app.get(
-  "/messages",
-  validateData(searchSchema),
-  async (req: Request, res: Response) => {
-    const searchTerms: string[] = req.body;
-    try {
-      console.log(req.body);
-      return res.status(200).json({ success: true });
-    } catch (e) {
-      console.log(e);
-    }
+app.get("/messages", async (req: Request, res: Response) => {
+  try {
+    const { tags } = req.body;
+    const tagsJoined = tags.join(" OR ");
+
+    const results = await db
+      .select({
+        messageId: messages.id,
+        messageText: messages.message,
+        channelName: channels.channel_name,
+      })
+      .from(messages)
+      .innerJoin(channels, eq(messages.channel_id, channels.id))
+      .where(sql`message @@@ ${tagsJoined}`);
+
+    return res.status(200).json({ success: true, data: results });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ success: false, e });
   }
-);
+});
 
 app.listen(port, () => {
   console.log(
