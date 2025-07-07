b85f15ab40cc9d0dc072ebc16e08da755e6077c6
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-07-07 07:26:59 +0300

    feat: install axios for data fetches

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import FetchTags from "./tags";
 import "./tailwind.css";
+import axios, { isCancel, AxiosError } from "axios";
 
 function App() {
   const [tags, setTags] = useState<string[]>([]);
+  const [data, setData] = useState<object[]>([]);
 
   const handleTagsChange = (updatedTags: string[]) => {
     setTags(updatedTags);
-    console.log("Tags in parent:", updatedTags);
   };
+
+  async function fetchData(e: React.FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    let tagsJoined = tags.join("&tags=");
+    try {
+      let data = axios.get(`http://localhost:3099?tags=${tagsJoined}`);
+      console.log(`http://localhost:3099?tags=${tagsJoined}`);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <div className="App">
       <h1 className="text-3xl"> Telegram search </h1>
       {/* get tags */}
       <FetchTags onChange={handleTagsChange} />
+
+      <div>
+        <button className="" onClick={fetchData}>
+          {" "}
+          Search Tags{" "}
+        </button>
+      </div>
     </div>
   );
 }
