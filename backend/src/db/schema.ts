5bebe97930547a2852bc2d8f2f7d52b28fd0b48c
Derrick_Munyiri <dmunyiri12@gmail.com>
2025-10-26 15:54:41 +0300

    feat: finish chart

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,168 +1,168 @@
 import {
-  integer,
-  pgTable,
-  varchar,
-  bigint,
-  timestamp,
-  text,
-  unique,
-  boolean,
+    integer,
+    pgTable,
+    varchar,
+    bigint,
+    timestamp,
+    text,
+    unique,
+    boolean,
 } from "drizzle-orm/pg-core";
 import { relations, sql } from "drizzle-orm";
 import { uniqueIndex } from "drizzle-orm/sqlite-core";
 
 export const channels = pgTable("channels", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  telegram_channel_id: bigint("telegram_channel_id", {
-    mode: "bigint",
-  }).unique(),
-  channel_name: varchar({ length: 255 }).notNull(),
-  created_at: timestamp({ precision: 6, withTimezone: true })
-    .notNull()
-    .defaultNow(),
-  updated_at: timestamp("updated_at")
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`)
-    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
-  deleted_at: timestamp("deleted_at"),
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    telegram_channel_id: bigint("telegram_channel_id", {
+        mode: "bigint",
+    }).unique(),
+    channel_name: varchar({ length: 255 }).notNull(),
+    created_at: timestamp({ precision: 6, withTimezone: true })
+        .notNull()
+        .defaultNow(),
+    updated_at: timestamp("updated_at")
+        .notNull()
+        .default(sql`CURRENT_TIMESTAMP`)
+        .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+    deleted_at: timestamp("deleted_at"),
 });
 
 export const channelsRelations = relations(channels, ({ many }) => ({
-  messages: many(messages),
+    messages: many(messages),
 }));
 
 export const channelRelationToBatches = relations(channels, ({ many }) => ({
-  batches: many(batches),
+    batches: many(batches),
 }));
 
 export const messages = pgTable(
-  "messages",
-  {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    message: text().notNull(),
-    telegram_message_id: integer(),
-    telegram_created_at: timestamp("telegram_created_at").notNull(),
-    created_at: timestamp("created_at").notNull().defaultNow(),
-    updated_at: timestamp("updated_at")
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`)
-      .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
-    deleted_at: timestamp("deleted_at"),
-    channel_id: bigint("channel_id", {
-      mode: "bigint",
-    })
-      .notNull()
-      .references(() => channels.id),
-  },
-  (t) => [
-    unique("channel_messages_unique")
-      .on(t.channel_id, t.telegram_message_id)
-      .nullsNotDistinct(),
-  ]
+    "messages",
+    {
+        id: integer().primaryKey().generatedAlwaysAsIdentity(),
+        message: text().notNull(),
+        telegram_message_id: integer(),
+        telegram_created_at: timestamp("telegram_created_at").notNull(),
+        created_at: timestamp("created_at").notNull().defaultNow(),
+        updated_at: timestamp("updated_at")
+            .notNull()
+            .default(sql`CURRENT_TIMESTAMP`)
+            .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+        deleted_at: timestamp("deleted_at"),
+        channel_id: bigint("channel_id", {
+            mode: "bigint",
+        })
+            .notNull()
+            .references(() => channels.id),
+    },
+    (t) => [
+        unique("channel_messages_unique")
+            .on(t.channel_id, t.telegram_message_id)
+            .nullsNotDistinct(),
+    ],
 );
 
 export const messagesRelations = relations(messages, ({ one }) => ({
-  channel: one(channels, {
-    fields: [messages.channel_id],
-    references: [channels.id],
-  }),
+    channel: one(channels, {
+        fields: [messages.channel_id],
+        references: [channels.id],
+    }),
 }));
 
 export const messagesRelationsToAnswers = relations(messages, ({ many }) => ({
-  answers: many(answers),
+    answers: many(answers),
 }));
 
 export const questions = pgTable("questions", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  question: text().notNull().unique(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
-  updated_at: timestamp("updated_at")
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`)
-    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
-  deleted_at: timestamp("deleted_at"),
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    question: text().notNull().unique(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at")
+        .notNull()
+        .default(sql`CURRENT_TIMESTAMP`)
+        .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+    deleted_at: timestamp("deleted_at"),
 });
 
 export const questionsRelationsToAnswers = relations(questions, ({ many }) => ({
-  answers: many(answers),
+    answers: many(answers),
 }));
 
 export const questionsRelationsToBatches = relations(questions, ({ many }) => ({
-  batches: many(batches),
+    batches: many(batches),
 }));
 
 export const answers = pgTable(
-  "answers",
-  {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    question_id: integer()
-      .notNull()
-      .references(() => questions.id),
-    message_id: integer()
-      .notNull()
-      .references(() => messages.id),
-    answer: text().notNull(),
-    created_at: timestamp("created_at").notNull().defaultNow(),
-    updated_at: timestamp("updated_at")
-      .notNull()
-      .default(sql`CURRENT_TIMESTAMP`)
-      .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
-    deleted_at: timestamp("deleted_at"),
-  },
-  (t) => [
-    unique("answers_unique_index")
-      .on(t.question_id, t.message_id)
-      .nullsNotDistinct(),
-  ]
+    "answers",
+    {
+        id: integer().primaryKey().generatedAlwaysAsIdentity(),
+        question_id: integer()
+            .notNull()
+            .references(() => questions.id),
+        message_id: integer()
+            .notNull()
+            .references(() => messages.id),
+        answer: text().notNull(),
+        created_at: timestamp("created_at").notNull().defaultNow(),
+        updated_at: timestamp("updated_at")
+            .notNull()
+            .default(sql`CURRENT_TIMESTAMP`)
+            .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+        deleted_at: timestamp("deleted_at"),
+    },
+    (t) => [
+        unique("answers_unique_index")
+            .on(t.question_id, t.message_id)
+            .nullsNotDistinct(),
+    ],
 );
 
 export const answersRelationsToMessages = relations(answers, ({ one }) => ({
-  message: one(messages, {
-    fields: [answers.message_id],
-    references: [messages.id],
-  }),
+    message: one(messages, {
+        fields: [answers.message_id],
+        references: [messages.id],
+    }),
 }));
 
 export const answersRelationsToQuestions = relations(answers, ({ one }) => ({
-  question: one(questions, {
-    fields: [answers.question_id],
-    references: [questions.id],
-  }),
+    question: one(questions, {
+        fields: [answers.question_id],
+        references: [questions.id],
+    }),
 }));
 
 export const batches = pgTable("batches", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  channel_id: bigint("channel_id", {
-    mode: "bigint",
-  })
-    .notNull()
-    .references(() => channels.id),
-  question_id: integer()
-    .notNull()
-    .references(() => questions.id),
-  batchid: text().notNull().unique(),
-  status: text().notNull(),
-  fileid: text().notNull().unique(),
-  outputfileid: text().unique(),
-  saved: boolean().default(false).notNull(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
-  updated_at: timestamp("updated_at", { withTimezone: true, mode: "string" })
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`)
-    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
-  deleted_at: timestamp("deleted_at"),
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    channel_id: bigint("channel_id", {
+        mode: "bigint",
+    })
+        .notNull()
+        .references(() => channels.id),
+    question_id: integer()
+        .notNull()
+        .references(() => questions.id),
+    batchid: text().notNull().unique(),
+    status: text().notNull(),
+    fileid: text().notNull().unique(),
+    outputfileid: text().unique(),
+    saved: boolean().default(false).notNull(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at", { withTimezone: true, mode: "string" })
+        .notNull()
+        .default(sql`CURRENT_TIMESTAMP`)
+        .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+    deleted_at: timestamp("deleted_at"),
 });
 
 export const batchRelationsToQuestions = relations(batches, ({ one }) => ({
-  question: one(questions, {
-    fields: [batches.question_id],
-    references: [questions.id],
-  }),
+    question: one(questions, {
+        fields: [batches.question_id],
+        references: [questions.id],
+    }),
 }));
 
 export const batchRelationsToChannels = relations(batches, ({ one }) => ({
-  channel: one(channels, {
-    fields: [batches.channel_id],
-    references: [channels.id],
-  }),
+    channel: one(channels, {
+        fields: [batches.channel_id],
+        references: [channels.id],
+    }),
 }));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,171 +3,124 @@ import FetchTags from "./tags";
 import "./tailwind.css";
 import axios from "axios";
 import {
-    LineChart,
-    Line,
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    Legend,
-    ResponsiveContainer,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
 } from "recharts";
 
 function App() {
-    const [tags, setTags] = useState<string[]>([]);
-    const [data, setData] = useState<object[]>([]);
-    const [sorted, setSorted] = useState<object[]>([]);
-    const [all_channels, setAllChannels] = useState<string[]>([]);
-    const [isPending, startTransition] = useTransition();
+  const [tags, setTags] = useState<string[]>([]);
+  const [data, setData] = useState<object[]>([]);
+  const [sorted, setSorted] = useState<object[]>([]);
+  const [all_channels, setAllChannels] = useState<string[]>([]);
+  const [isPending, startTransition] = useTransition();
 
-    async function getData() {
-        let tagsJoined = tags.join("&tags=");
-        try {
-            let data = await axios.get(
-                `http://localhost:3099/messages?tags=${tagsJoined}`,
-            );
-            startTransition(() => {
-                setData(data.data);
-                sortData(data.data);
-            });
-        } catch (e) {
-            console.log(e);
-        }
+  async function getData() {
+    let tagsJoined = tags.join("&tags=");
+    try {
+      let data = await axios.get(
+        `http://localhost:3099/messages?tags=${tagsJoined}`,
+      );
+      startTransition(() => {
+        setData(data.data);
+        sortData(data.data);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    if (tags.length > 0) {
+      getData();
+    } else {
+      setSorted([]);
+    }
+  }, [tags]);
+
+  const handleTagsChange = async (updatedTags: string[]) => {
+    setTags(updatedTags);
+  };
+
+  const sortData = (data: {
+    success: boolean;
+    data: {
+      messageId: number;
+      messageText: string;
+      channelName: string;
+      created_at: string;
+    }[];
+  }) => {
+    const messages = data.data;
+    const grouped = new Map();
+
+    for (const msg of messages) {
+      const { created_at, channelName } = msg;
+      if (!grouped.has(created_at))
+        grouped.set(created_at, { date: created_at });
+      const entry = grouped.get(created_at);
+      entry[channelName] = (entry[channelName] || 0) + 1;
     }
 
-    useEffect(() => {
-        if (tags.length > 0) {
-            getData();
-        } else {
-            setSorted([]);
-        }
-    }, [tags]);
+    const sortedData = Array.from(grouped.values());
+    const channels = Array.from(new Set(messages.map((m) => m.channelName)));
 
-    const handleTagsChange = async (updatedTags: string[]) => {
-        setTags(updatedTags);
-    };
+    setAllChannels(channels);
+    setSorted(sortedData);
+  };
 
-    const sortData = (data: {
-        success: boolean;
-        data: {
-            messageId: number;
-            messageText: string;
-            channelName: string;
-            created_at: string;
-        }[];
-    }) => {
-        const messages = data.data;
-        const grouped = new Map();
+  async function fetchData(e: React.FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    await getData();
+  }
 
-        for (const msg of messages) {
-            const { created_at, channelName } = msg;
-            if (!grouped.has(created_at))
-                grouped.set(created_at, { date: created_at });
-            const entry = grouped.get(created_at);
-            entry[channelName] = (entry[channelName] || 0) + 1;
-        }
+  return (
+    <div className="App">
+      <h1 className="text-3xl"> Telegram search </h1>
+      {/* get tags */}
+      <FetchTags onChange={handleTagsChange} />
 
-        const sortedData = Array.from(grouped.values());
-        const channels = Array.from(
-            new Set(messages.map((m) => m.channelName)),
-        );
+      <div>
+        <button className="" onClick={fetchData}>
+          {" "}
+          Search Tags{" "}
+        </button>
+      </div>
+      {/*render chart as long as sorted is true*/}
 
-        setAllChannels(channels);
-        setSorted(sortedData);
-    };
-
-    // const sortData = (data: {
-    //     success: boolean;
-    //     data: {
-    //         messageId: number;
-    //         messageText: string;
-    //         channelName: string;
-    //         created_at: string;
-    //     }[];
-    // }) => {
-    //     // receives the data and sorts it in a format that can be charted. Send to db as json
-    //     let channels = Array.from(
-    //         new Set(data.data.map((dt) => dt.channelName)),
-    //     );
-    //     setAllChannels(channels);
-    //     let dates = Array.from(new Set(data.data.map((dt) => dt.created_at)));
-    //     let sortedData = [];
-
-    //     // for each date, go through the dataset. For each channel, find the number of mentions per day
-    //     for (let date of dates) {
-    //         let obj = { date } as Record<string, number> & { date: string };
-    //         obj.date = date;
-    //         for (let chan of channels) {
-    //             let mentions = data.data.filter(
-    //                 (x) => x.created_at === date && x.channelName === chan,
-    //             ).length;
-
-    //             obj[chan] = mentions;
-    //         }
-    //         sortedData.push(obj);
-    //     }
-
-    //     // console.log(sortedData);
-
-    //     setSorted(sortedData);
-    // };
-
-    async function fetchData(e: React.FormEvent<HTMLButtonElement>) {
-        e.preventDefault();
-        await getData();
-    }
-
-    return (
-        <div className="App">
-            <h1 className="text-3xl"> Telegram search </h1>
-            {/* get tags */}
-            <FetchTags onChange={handleTagsChange} />
-
-            <div>
-                <button className="" onClick={fetchData}>
-                    {" "}
-                    Search Tags{" "}
-                </button>
-            </div>
-            {/*render chart as long as sorted is true*/}
-
-            <div style={{ width: "100%", height: 400 }}>
-                <ResponsiveContainer>
-                    <LineChart
-                        data={sorted}
-                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis
-                            dataKey="date"
-                            tickFormatter={(d) => d.split("T")[0]}
-                        />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        {all_channels.map((chan, i) => (
-                            <Line
-                                // key={chan}
-                                type="monotone"
-                                dataKey={chan}
-                                strokeWidth={4}
-                                stroke={
-                                    [
-                                        "#8884d8",
-                                        "#82ca9d",
-                                        "#ff7300",
-                                        "#00C49F",
-                                        "#FFBB28",
-                                    ][i % 5]
-                                }
-                                dot={false}
-                            />
-                        ))}
-                    </LineChart>
-                </ResponsiveContainer>
-            </div>
-        </div>
-    );
+      <div style={{ width: "100%", height: 400 }}>
+        <ResponsiveContainer>
+          <LineChart
+            data={sorted}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" tickFormatter={(d) => d.split("T")[0]} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {all_channels.map((chan, i) => (
+              <Line
+                // key={chan}
+                type="monotone"
+                dataKey={chan}
+                strokeWidth={4}
+                stroke={
+                  ["#8884d8", "#82ca9d", "#ff7300", "#00C49F", "#FFBB28"][i % 5]
+                }
+                dot={false}
+              />
+            ))}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
